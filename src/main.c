7ec22abd61e90981bb1b5b17e0208a46c3dbc7d5
ravard <avardrobin@hotmail.com>
2020-03-27 01:01:32 +0100

    usage is in varint_test.sh script

--- src/main.c
+++ src/main.c
@@ -1,49 +1,10 @@
 #include "libft.h"
 #include "varint_test.h"
 
-static void			type_usage( void )
-{
-	static char		*type[] = {"op", "u64_cmp", "nnaumenk_cmp", "find_prime", "asn1_der"};
-
-	for (int i = 0; i < 4; i++)
-		ft_printf("\n%s%s%s:\n", KBLU, type[i], KNRM);
-			
-}
-
-
-//static void			opts_usage( void )
-//{
-//	static char		*opts[] = {"end0",
-//									P, Q, R, S, "end1",
-//									E, D, I, O,
-//									DES_ONLY, A, K, V, SALT, PW, "end2"};
-//	int				i;
-//
-//	i = -1;
-//	ft_printf("\n%sStandard opts%s:\n", KYEL, KNRM);
-//	while (ft_strcmp("end0", opts[++i]))
-//		ft_printf(" %s\n", opts[i]);
-//	ft_printf("\n%sMessage Digest opts%s:\n", KYEL, KNRM);
-//	while (ft_strcmp("end1", opts[++i]))
-//		ft_printf(" %s\n", opts[i]);
-//	ft_printf("\n%sCipher opts%s:\n", KYEL, KNRM);
-//	while (ft_strcmp("end2", opts[++i]))
-//		ft_printf(" %s\n", opts[i]);
-
-# define U "%susage%s: ./varint_test %stype %s[opts]%s [args]\n"
-
-int			usage(void)
-{
-	ft_printf(U, KGRN, KWHT, KBLU, KYEL, KWHT);
-	type_usage();
-//	opts_usage();
-	return (-1);
-}
-
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		return (usage());
+		return (ft_printf("%sfor usage run: sh varint_test.sh%s\n", KYEL, KNRM));
 	else if (!ft_strcmp("op", argv[1]) && argc == 4)
 		return (op(argv));
 	else if (!ft_strcmp("u64_cmp", argv[1]) && argc == 4)
